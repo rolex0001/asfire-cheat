b892d6ae53a85f8fdc846d606c9f1fe42ed63a8f
Pradhuman2623 <90610809+Pradhuman2623@users.noreply.github.com>
2022-10-03 00:16:55 +0530

    Update Modules.cpp

--- Dainsleif/Modules.cpp
+++ Dainsleif/Modules.cpp
@@ -11,4 +11,34 @@ namespace Modules
         client = reinterpret_cast<uintptr_t>(GetModuleHandle("client.dll"));
         engine = reinterpret_cast<uintptr_t>(GetModuleHandle("engine.dll"));
     }
+    
+    bool tuxljpe::fjbryjealwnpkux(bool hxhdphkrf, int njiqzyiuf) {
+double dbhyxykamxmjrg = 19437;
+string uokps = "mzyeailiwtwntqdrmruauledzg";
+double ddhvtoybkayjvqn = 31631;
+double uvvqt = 37172;
+bool eoagoxyfumuyau = false;
+string lqohatacx = "vaxogzgjrmrmemroadoavnzduweqiqzfpyahqerixdbfnfyerogknunxoybhrf";
+string cpysr = "zbzxpywvwqkoauekefdmgpeugfibvlcpjboahllqucjvbdulbbnvrlxwktuklazdogwfrnmkwmhvl";
+int jfkagod = 271;
+if (false == false) {
+int tovzhh;
+for (tovzhh=62; tovzhh > 0; tovzhh--) {
+continue;
+} 
+}
+if (false == false) {
+int tsfpshxzuy;
+for (tsfpshxzuy=86; tsfpshxzuy > 0; tsfpshxzuy--) {
+continue;
+} 
+}
+if (string("mzyeailiwtwntqdrmruauledzg") == string("mzyeailiwtwntqdrmruauledzg")) {
+int lc;
+for (lc=35; lc > 0; lc--) {
+continue;
+} 
+}
+return false;
+}
 }
